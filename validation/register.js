c33fd7e7e546b8eb66f974fc53cb91d61d41c08a
Kreattehak <nerokamil@gmail.com>
2018-06-01 22:14:36 +0200

    Add server-side validation for login and register form

---
+++ validation/register.js
@@ -0,0 +1,41 @@
+const Validator = require("validator");
+const isEmpty = require("./emptyObjectValidator");
+
+module.exports = function validateUserRegisterInput(data) {
+  let errors = {};
+
+  data.name = !isEmpty(data.name) ? data.name : "";
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.confirmedPassword = !isEmpty(data.confirmedPassword) ? data.confirmedPassword : "";
+
+  if (Validator.isEmpty(data.name)) {
+    console.log("Username field is required");
+    errors.name = "Username field is required!";
+  } else if (!Validator.isLength(data.name, { min: 4, max: 30 })) {
+    errors.name = "Username length must be between 4 and 30 characters!";
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required!";
+  } else if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is not valid!";
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required!";
+  } else if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = "Password length must be between 6 and 30 characters!";
+  }
+
+  if (Validator.isEmpty(data.confirmedPassword)) {
+    errors.confirmedPassword = "Confirmed password field is required!";
+  } else if (!Validator.equals(data.password, data.confirmedPassword)) {
+    errors.confirmedPassword = "Passwords do not match!";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
