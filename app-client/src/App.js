cab47d6d82166647640b676e5ab96bfb441d03c3
Kreattehak <nerokamil@gmail.com>
2018-06-06 16:48:04 +0200

    Add simple user side routing

--- app-client/src/App.js
+++ app-client/src/App.js
@@ -1,17 +1,24 @@
 import React, {Component} from 'react';
 import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
+import Login from './components/authentication/Login';
+import Register from './components/authentication/Register';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <Navbar/>
-                <Landing/>
-                <Footer/>
-            </div>
+            <Router>
+                <div className="App">
+                    <Navbar/>
+                    <Route path="/" component={Landing} exact/>
+                    <Route path="/login" component={Login} exact/>
+                    <Route path="/register" component={Register} exact/>
+                    <Footer/>
+                </div>
+            </Router>
         );
     }
 }
