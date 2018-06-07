d756ee334fe71f64511f8c5e9b32850f9c730a3e
Kreattehak <nerokamil@gmail.com>
2018-06-07 13:13:16 +0200

    Extract starting App component and wrap it with Provider

--- app-client/src/index.js
+++ app-client/src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import store from "./store/store";
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = (
+  <Provider store={store}>
+    <App/>
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
