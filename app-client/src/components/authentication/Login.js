88a817e7448bb69a7226e37b7d7f7ad66e6f31d1
Kreattehak <nerokamil@gmail.com>
2018-06-08 07:37:30 +0200

    Connect Login component with react-redux

--- app-client/src/components/authentication/Login.js
+++ app-client/src/components/authentication/Login.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
+import classnames from 'classnames';
+import {connect} from 'react-redux';
+import * as authActions from '../../store/actions/auth';
+import * as errorActions from '../../store/actions/error';
+import PropTypes from 'prop-types';
 
 class Login extends Component {
   state = {
     email: '',
     password: '',
-    errors: {}
   };
+
+  componentDidMount() {
+    this.props.cleanUpErrors();
+  }
 
   onChange = (event) => {
     this.setState({[event.target.name]: event.target.value});
@@ -16,10 +24,12 @@ class Login extends Component {
     const userData = {
       ...this.state,
     };
-    delete userData['errors'];
+
+    this.props.initLoginUser(userData, this.props.history);
   };
 
   render() {
+    const {errors} = this.props;
     return (
       <div className="login">
         <div className="container">
@@ -29,14 +39,24 @@ class Login extends Component {
               <p className="lead text-center">Sign in to your DevConnector account</p>
               <form onSubmit={this.onSubmit}>
                 <div className="form-group">
-                  <input type="email" className="form-control form-control-lg"
-                         placeholder="Email Address" name="email" autoComplete="email"
-                         value={this.state.email} onChange={this.onChange}/>
+                  <input type="email"
+                         className={classnames('form-control form-control-lg', {'is-invalid': errors.email})}
+                         placeholder="Email Address"
+                         name="email"
+                         autoComplete="email"
+                         value={this.state.email}
+                         onChange={this.onChange}/>
+                  {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
                 </div>
                 <div className="form-group">
-                  <input type="password" className="form-control form-control-lg"
-                         placeholder="Password" name="password" autoComplete="current-password"
-                         value={this.state.password} onChange={this.onChange}/>
+                  <input type="password"
+                         className={classnames('form-control form-control-lg', {'is-invalid': errors.password})}
+                         placeholder="Password"
+                         name="password"
+                         autoComplete="current-password"
+                         value={this.state.password}
+                         onChange={this.onChange}/>
+                  {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4"/>
               </form>
@@ -48,4 +68,26 @@ class Login extends Component {
   }
 }
 
-export default Login;
+Login.propTypes = {
+  initLoginUser: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
+  user: PropTypes.object.isRequired,
+  errors: PropTypes.object
+};
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.isAuthenticated,
+    user: state.auth.user,
+    errors: state.errors
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    initLoginUser: (payload, history) => dispatch(authActions.initLoginUser(payload, history)),
+    cleanUpErrors: () => dispatch(errorActions.cleanUpErrors())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
