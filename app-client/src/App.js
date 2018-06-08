e1eac2a7692799f93b6921371e94de37d1f6d2c1
Kreattehak <nerokamil@gmail.com>
2018-06-08 07:24:50 +0200

    Add automatic login when user has jwt token in local storage

--- app-client/src/App.js
+++ app-client/src/App.js
@@ -4,8 +4,18 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Login from './components/authentication/Login';
 import Register from './components/authentication/Register';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import {addAuthorizationHeader} from './utils/utils';
+import {loginUser} from './store/actions/auth';
+import store from './store/store';
 import './App.css';
+
+const token = localStorage.getItem('JWT_TOKEN');
+if (token) {
+  addAuthorizationHeader(token);
+  store.dispatch(loginUser(jwt_decode(token)));
+}
 
 class App extends Component {
   render() {
