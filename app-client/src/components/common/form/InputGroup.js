37bd222f10f2a64516802d3378a0fc5b55ef4bbf
Kreattehak <nerokamil@gmail.com>
2018-06-11 08:26:02 +0200

    Add components which represents various form fields

--- app-client/src/components/common/form/InputGroup.js
+++ app-client/src/components/common/form/InputGroup.js
@@ -2,49 +2,48 @@ import React from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
-const InputFieldGroup =
+const InputGroup =
   ({
      autoComplete,
-     disabled,
      error,
-     info,
+     icon,
      name,
      onChange,
      placeholder,
-     required,
      type,
      value
    }) => (
-    <div className="form-group">
-      <input autoComplete={autoComplete}
+    <div className="input-group mb-3">
+      <div className="input-group-prepend">
+        <span className="input-group-text">
+          <i className={icon}/>
+        </span>
+      </div>
+      <input autoComplete={autoComplete} //TODO: Should use InputFieldGroup?
              className={classnames('form-control form-control-lg', {'is-invalid': error})}
-             disabled={disabled}
              name={name}
              onChange={onChange}
              placeholder={placeholder}
-             required={required}
              type={type}
              value={value}/>
-      {info && <small className="form-text text-muted">{info}</small>}
       {error && (<div className="invalid-feedback">{error}</div>)}
     </div>
   );
 
-InputFieldGroup.propTypes = {
+InputGroup.propTypes = {
   autoComplete: PropTypes.string,
-  disabled: PropTypes.bool,
   error: PropTypes.string,
-  info: PropTypes.string,
+  icon: PropTypes.string,
   name: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
   placeholder: PropTypes.string,
-  required: PropTypes.bool,
   type: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired
 };
 
-InputFieldGroup.defaultProps = {
+InputGroup.defaultProps = {
+  autoComplete: 'off',
   type: 'text'
 };
 
-export default InputFieldGroup;
+export default InputGroup;
---
+++ app-client/src/components/common/form/SelectGroup.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+
+const SelectGroup =
+  ({
+     error,
+     info,
+     name,
+     onChange,
+     options,
+     value
+   }) => {
+    const optionsToSelect = options.map(option => (
+      <option key={option.label} value={option.value}>{option.label}</option>
+    ));
+    return (
+      <div className="form-group">
+        <select className={classnames('form-control form-control-lg', {'is-invalid': error})}
+                name={name}
+                onChange={onChange}
+                value={value}>
+          {optionsToSelect}
+        </select>
+        {info && <small className="form-text text-muted">{info}</small>}
+        {error && (<div className="invalid-feedback">{error}</div>)}
+      </div>
+    )
+  };
+
+SelectGroup.propTypes = {
+  error: PropTypes.string,
+  info: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  options: PropTypes.array.isRequired,
+  value: PropTypes.string.isRequired
+};
+
+export default SelectGroup;
