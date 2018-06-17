f8f9fe37ef489bdb1e6236c0bd562ab4e719097a
Kreattehak <nerokamil@gmail.com>
2018-06-17 15:40:42 +0200

    Refactor error text response handle in browserErrorMiddleware

--- app-client/src/store/middleware/browserError.js
+++ app-client/src/store/middleware/browserError.js
@@ -1,17 +1,5 @@
-import * as err from '../actions/actionTypes';
-const errorActions = [
-  err.SAVE_NEW_USER_REQUEST_NOT_PROCESSED,
-  err.LOGIN_USER_REQUEST_NOT_PROCESSED,
-  err.CREATE_PROFILE_REQUEST_NOT_PROCESSED,
-  err.UPDATE_PROFILE_REQUEST_NOT_PROCESSED,
-  err.REMOVE_PROFILE_ALONG_WITH_USER_REQUEST_NOT_PROCESSED,
-  err.ADD_EXPERIENCE_REQUEST_NOT_PROCESSED,
-  err.REMOVE_EXPERIENCE_REQUEST_NOT_PROCESSED,
-  err.ADD_EDUCATION_REQUEST_NOT_PROCESSED,
-];
 const browserErrorMiddleware = store => next => action => {
-  let isErrorAction = errorActions.filter(type => action.type === type).length !== 0;
-  if (isErrorAction) {
+  if (typeof action.errors === 'string') {
     action.errors = {browserError: action.errors};
   }
   return next(action);
