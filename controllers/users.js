d86bce471d4a25fdde8907f728fe6906d861daaf
Kreattehak <nerokamil@gmail.com>
2018-06-26 17:33:24 +0200

    Add async/await in backend for functions that works with user model

--- controllers/users.js
+++ controllers/users.js
@@ -7,50 +7,45 @@ const validateUserRegisterInput = require("../validation/register");
 const validateUserLoginInput = require("../validation/login");
 
 function getCurrentUser(req, res) {
-  return res.json(req.user);
+  res.json(req.user);
 }
 
-function addUser(req, res) {
+async function addUser(req, res) {
   const {errors, isValid} = validateUserRegisterInput(req.body);
 
   if (!isValid) {
     return res.status(400).json(errors);
   }
 
-  User.findOne({email: req.body.email})
-    .then(user => {
-      if (user) {
-        errors.email = "Email already in use!";
-        return res.status(400).json(errors);
-      } else {
-        const avatar = gravatar.url(req.body.email, {
-          s: "200", // size
-          r: "pg", // rating
-          d: "mm" // default
-        });
-        const newUser = new User({
-          name: req.body.name,
-          email: req.body.email,
-          password: req.body.password,
-          avatar
-        });
+  const user = await User.findOne({email: req.body.email});
+  if (user) {
+    errors.email = "Email already in use!";
+    res.status(400).json(errors);
+  } else {
+    const avatar = gravatar.url(req.body.email, {
+      s: "200", // size
+      r: "pg", // rating
+      d: "mm" // default
+    });
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      avatar
+    });
 
-        bcrypt.genSalt(10, (error, salt) => {
-          bcrypt.hash(newUser.password, salt, (error, hash) => {
-            if (error) throw error;
-            newUser.password = hash;
-            newUser
-              .save()
-              .then(user => res.json(user))
-              .catch(error => res.status(404).json(error));
-          });
-        });
-      }
-    })
-    .catch(error => res.status(404).json(error));
+    bcrypt.genSalt(10, (error, salt) => {
+      bcrypt.hash(newUser.password, salt, async (error, hash) => {
+        if (error) throw error;
+        newUser.password = hash;
+        const savedUser = await newUser.save();
+        res.json(savedUser);
+      });
+    });
+  }
 }
 
-function loginUser(req, res) {
+async function loginUser(req, res) {
   const email = req.body.email;
   const password = req.body.password;
   const {errors, isValid} = validateUserLoginInput({email, password});
@@ -59,48 +54,40 @@ function loginUser(req, res) {
     return res.status(400).json(errors);
   }
 
-  User.findOne({email})
-    .then(user => {
-      if (!user) {
-        errors.email = "Email not found!";
-        return res.status(404).json(errors);
-      }
+  const user = await User.findOne({email});
+  if (!user) {
+    errors.email = "Email not found!";
+    return res.status(404).json(errors);
+  }
 
-      bcrypt.compare(password, user.password).then(isMatch => {
-        if (isMatch) {
-          const payload = {
-            id: user.id,
-            name: user.name,
-            avatar: user.avatar
-          };
-          jwt.sign(payload, process.env.SECRET_JWT_TOKEN, {expiresIn: 3600}, (error, token) => (
-            res.json({
-              success: true,
-              token: "Bearer " + token
-            })
-          ));
-        } else {
-          errors.password = "Password or email is invalid!";
-          return res.status(404).json(errors);
-        }
-      });
-    })
-    .catch(error => res.status(404).json(error));
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (isMatch) {
+    const payload = {
+      id: user.id,
+      name: user.name,
+      avatar: user.avatar
+    };
+    jwt.sign(payload, process.env.SECRET_JWT_TOKEN, {expiresIn: 3600}, (error, token) => (
+      res.json({
+        success: true,
+        token: "Bearer " + token
+      })
+    ));
+  } else {
+    errors.password = "Password or email is invalid!";
+    res.status(404).json(errors);
+  }
 }
 
-function deleteUser(req, res) {
+async function deleteUser(req, res) {
   const errors = {};
-  User.findOneAndRemove({_id: req.user.id})
-    .then((profile) => {
-      if (!profile) {
-        errors.profile = "There is no user with given id in database!";
-        return res.status(404).json(errors);
-      }
-      Profile.findOneAndRemove({user: req.user.id})
-        .then(() => res.json({userWasDeleted: true}))
-        .catch(error => res.status(404).json(error));
-    })
-    .catch(error => res.status(404).json(error));
+  const profile = await User.findOneAndRemove({_id: req.user.id});
+  if (!profile) {
+    errors.profile = "There is no user with given id in database!";
+    return res.status(404).json(errors);
+  }
+  await Profile.findOneAndRemove({user: req.user.id});
+  res.json({userWasDeleted: true});
 }
 
 module.exports = {
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const usersController = require('../../controllers/users');
+const catchAsync = require('../../middleware/error');
 
 /**
  * @route GET api/users/current
@@ -15,20 +16,20 @@ router.get("/current", passport.authenticate("jwt", {session: false}), usersCont
  * @desc Registers user
  * @access Public
  **/
-router.post("/register", usersController.addUser);
+router.post("/register", catchAsync(usersController.addUser));
 
 /**
  * @route POST api/users/login
  * @desc Returns JSON Web Token to login user
  * @access Public
  **/
-router.post("/login", usersController.loginUser);
+router.post("/login", catchAsync(usersController.loginUser));
 
 /**
  * @route DELETE api/users/
  * @desc Deletes user along with profile
  * @access Private
  **/
-router.delete('/', passport.authenticate("jwt", {session: false}), usersController.deleteUser);
+router.delete('/', passport.authenticate("jwt", {session: false}), catchAsync(usersController.deleteUser));
 
 module.exports = router;
