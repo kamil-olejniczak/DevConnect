1c28cf86c8d80337beb9a69c2678ca32c1fef98d
Kreattehak <nerokamil@gmail.com>
2018-06-28 19:10:34 +0200

    Refactor post controller logic to use async/await

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,37 +2,31 @@ const Post = require("../models/Post");
 const Profile = require("../models/Profile");
 const validateUserPostInput = require("../validation/post");
 
-function getPosts(req, res) {
-  Post.find()
-    .sort({date: -1})
-    .then(posts => {
-      if (posts.length === 0) {
-        errors.posts = "No posts found!";
-        return res.status(404).json(errors);
-      }
-      return res.json(posts);
-    })
-    .catch(error => res.status(404).json(error));
+async function getPosts(req, res) {
+  const errors = {};
+  const posts = await Post.find().sort({date: -1});
+  if (posts.length === 0) {
+    errors.posts = "No posts found!";
+    return res.status(404).json(errors);
+  }
+  res.json(posts);
 }
 
-function getPostById(req, res) {
+async function getPostById(req, res) {
   const errors = {};
   if (!req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
     errors.id = "Given id is not proper!";
     return res.status(404).json(errors);
   }
-  Post.findById(req.params.id)
-    .then(post => {
-      if (!post) {
-        errors.post = "Post with given id was nof found!";
-        return res.status(404).json(errors);
-      }
-      return res.json(post);
-    })
-    .catch(error => res.status(404).json(error));
+  const post = await Post.findById(req.params.id);
+  if (!post) {
+    errors.post = "Post with given id was nof found!";
+    return res.status(404).json(errors);
+  }
+  res.json(post);
 }
 
-function addPost(req, res) {
+async function addPost(req, res) {
   const {errors, isValid} = validateUserPostInput(req.body);
   if (!isValid) {
     return res.status(400).json(errors);
@@ -45,83 +39,68 @@ function addPost(req, res) {
     user: req.user.id
   });
 
-  newPost
-    .save()
-    .then(post => res.json(post))
-    .catch(error => res.status(404).json(error));
+  const post = await newPost.save();
+  res.json(post);
 }
 
-function likePostById(req, res) {
+async function likePostById(req, res) {
   const errors = {};
   if (!req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
     errors.id = "Given id is not proper!";
     return res.status(404).json(errors);
   }
-  Profile.findOne({user: req.user.id})
-    .then(profile => {
-      if (!profile) {
-        errors.profile = "There is no profile with given id in database!";
-        return res.status(404).json(errors);
-      }
-      Post.findById(req.params.id)
-        .then(post => {
-          if (post === null) {
-            errors.post = "Post with given id was nof found!";
-            return res.status(401).json(errors);
-          }
-          if (userAlreadyLikedThatPost(post, req.user.id)) {
-            errors.like = "Post already liked!";
-            return res.status(400).json(errors);
-          }
+  const profile = await Profile.findOne({user: req.user.id});
+  if (!profile) {
+    errors.profile = "There is no profile with given id in database!";
+    return res.status(404).json(errors);
+  }
+  const post = await Post.findById(req.params.id);
+  if (post === null) {
+    errors.post = "Post with given id was nof found!";
+    return res.status(401).json(errors);
+  }
+  if (userAlreadyLikedThatPost(post, req.user.id)) {
+    errors.like = "Post already liked!";
+    return res.status(400).json(errors);
+  }
 
-          post.likes.unshift({user: req.user.id});
-          post.save()
-            .then(post => res.json(post))
-            .catch(error => res.status(404).json(error));
-        })
-        .catch(error => res.status(404).json(error));
-    })
-    .catch(error => res.status(404).json(error));
+  post.likes.unshift({user: req.user.id});
+  await post.save();
+  const posts = await Post.find().sort({date: -1});
+  res.json(posts);
 }
 
-function unlikePostById(req, res) {
+async function unlikePostById(req, res) {
   const errors = {};
   if (!req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
     errors.id = "Given id is not proper!";
     return res.status(404).json(errors);
   }
-  Profile.findOne({user: req.user.id})
-    .then(profile => {
-      if (!profile) {
-        errors.profile = "There is no profile with given id in database!";
-        return res.status(404).json(errors);
-      }
-      Post.findById(req.params.id)
-        .then(post => {
-          if (post === null) {
-            errors.post = "Post with given id was nof found!";
-            return res.status(401).json(errors);
-          }
-          if (!userAlreadyLikedThatPost(post, req.user.id)) {
-            errors.like = "Post wasn't liked before!";
-            return res.status(400).json(errors);
-          }
+  const profile = await Profile.findOne({user: req.user.id});
+  if (!profile) {
+    errors.profile = "There is no profile with given id in database!";
+    return res.status(404).json(errors);
+  }
+  const post = await Post.findById(req.params.id);
+  if (post === null) {
+    errors.post = "Post with given id was nof found!";
+    return res.status(401).json(errors);
+  }
+  if (!userAlreadyLikedThatPost(post, req.user.id)) {
+    errors.like = "Post wasn't liked before!";
+    return res.status(400).json(errors);
+  }
 
-          const indexToBeRemoved = post.likes
-            .map(like => like.user.toString())
-            .indexOf(req.user.id);
+  const indexToBeRemoved = post.likes
+    .map(like => like.user.toString())
+    .indexOf(req.user.id);
 
-          post.likes.splice(indexToBeRemoved, 1);
-          post.save()
-            .then(post => res.json(post))
-            .catch(error => res.status(404).json(error));
-        })
-        .catch(error => res.status(404).json(error));
-    })
-    .catch(error => res.status(404).json(error));
+  post.likes.splice(indexToBeRemoved, 1);
+  const savedPost = await post.save();
+  res.json(savedPost);
 }
 
-function addCommentToPostById(req, res) {
+async function addCommentToPostById(req, res) {
   if (!req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
     return res.status(404).json("Given id is not proper!");
   }
@@ -131,87 +110,78 @@ function addCommentToPostById(req, res) {
     return res.status(400).json(errors);
   }
 
-  Post.findById(req.params.id)
-    .then(post => {
-      if (!post) {
-        errors.post = "Post with given id was nof found!";
-        return res.status(404).json(errors);
-      }
-      const newComment = {
-        text: req.body.text,
-        name: req.body.name,
-        avatar: req.body.avatar,
-        user: req.user.id
-      };
+  const post = await Post.findById(req.params.id);
+  if (!post) {
+    errors.post = "Post with given id was nof found!";
+    return res.status(404).json(errors);
+  }
+  const newComment = {
+    text: req.body.text,
+    name: req.body.name,
+    avatar: req.body.avatar,
+    user: req.user.id
+  };
 
-      post.comments.unshift(newComment);
-      post.save()
-        .then(post => res.json(post))
-        .catch(error => res.status(404).json(error));
-
-    })
-    .catch(error => res.status(404).json(error));
+  post.comments.unshift(newComment);
+  const savedPost = await post.save();
+  res.json(savedPost);
 }
 
-function deletePostById(req, res) {
+async function deletePostById(req, res) {
   const errors = {};
   if (!req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
     errors.id = "Given id is not proper!";
     return res.status(404).json(errors);
   }
-  Profile.findOne({user: req.user.id})
-    .then(profile => {
-      if (!profile) {
-        errors.profile = "There is no profile with given id in database!";
-        return res.status(404).json(errors);
-      }
-      Post.findById(req.params.id)
-        .then(post => {
-          if (post === null) {
-            errors.post = "Post with given id was nof found!";
-            return res.status(401).json(errors);
-          }
-          if (post.user.toString() !== req.user.id) {
-            errors.unauthorized = "User is not authorized to delete given post!";
-            return res.status(401).json(errors);
-          }
-          post.remove()
-            .then(() => res.json({postWasDeleted: true}));
-        })
-        .catch(error => res.status(404).json(error));
-    })
-    .catch(error => res.status(404).json(error));
+  const profile = await Profile.findOne({user: req.user.id});
+  if (!profile) {
+    errors.profile = "There is no profile with given id in database!";
+    return res.status(404).json(errors);
+  }
+  const post = await Post.findById(req.params.id);
+  if (post === null) {
+    errors.post = "Post with given id was nof found!";
+    return res.status(401).json(errors);
+  }
+  if (post.user.toString() !== req.user.id) {
+    errors.unauthorized = "User is not authorized to delete given post!";
+    return res.status(401).json(errors);
+  }
+
+  await post.remove();
+  const posts = await Post.find();
+  if (posts.length === 0) {
+    errors.posts = "No posts found!";
+    return res.status(404).json(errors);
+  }
+  res.json({postWasDeleted: true});
 }
 
-function deleteCommentByPostThenByCommentId(req, res) {
+async function deleteCommentByPostThenByCommentId(req, res) {
   const errors = {};
   if (!req.params.id.match(/^[0-9a-fA-F]{24}$/) || !req.params.comment_id.match(/^[0-9a-fA-F]{24}$/)) {
     errors.id = "Given id is not proper!";
     return res.status(404).json(errors);
   }
 
-  Post.findById(req.params.id)
-    .then(post => {
-      if (!post) {
-        errors.post = "There is no post with given id in database!";
-        return res.status(404).json(errors);
-      }
+  const post = await Post.findById(req.params.id);
+  if (!post) {
+    errors.post = "There is no post with given id in database!";
+    return res.status(404).json(errors);
+  }
 
-      if (commentDoesNotExist(post, req.params.comment_id)) {
-        errors.comment = "There is no comment with given id in database!";
-        return res.status(404).json(errors);
-      }
+  if (commentDoesNotExist(post, req.params.comment_id)) {
+    errors.comment = "There is no comment with given id in database!";
+    return res.status(404).json(errors);
+  }
 
-      const indexToBeRemoved = post.comments
-        .map(comment => comment.id)
-        .indexOf(req.params.comment_id);
+  const indexToBeRemoved = post.comments
+    .map(comment => comment.id)
+    .indexOf(req.params.comment_id);
 
-      post.comments.splice(indexToBeRemoved, 1);
-      post.save()
-        .then(() => res.json({commentWasDeleted: true}))
-        .catch(error => res.status(404).json(error))
-    })
-    .catch(error => res.status(404).json(error));
+  post.comments.splice(indexToBeRemoved, 1);
+  await post.save();
+  res.json({commentWasDeleted: true});
 }
 
 function userAlreadyLikedThatPost(post, userId) {
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,55 +2,57 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const postsController = require('../../controllers/posts');
+const catchAsync = require('../../middleware/error');
 
 /**
  * @route GET api/posts
  * @desc Returns posts
  * @access Public
  **/
-router.get('/', postsController.getPosts);
+router.get('/', catchAsync(postsController.getPosts));
 
 /**
  * @route GET api/posts/:id
  * @desc Returns post by id
  * @access Public
  **/
-router.get('/:id', postsController.getPostById);
+router.get('/:id', catchAsync(postsController.getPostById));
 
 /**
  * @route POST api/posts
  * @desc Posts new user post
  * @access Private
  **/
-router.post('/', passport.authenticate("jwt", {session: false}), postsController.addPost);
+router.post('/', passport.authenticate("jwt", {session: false}), catchAsync(postsController.addPost));
 
 /**
  * @route POST api/posts/like/:id
  * @desc Likes post with specified id
  * @access Private
  **/
-router.post('/like/:id', passport.authenticate("jwt", {session: false}), postsController.likePostById);
+router.post('/like/:id', passport.authenticate("jwt", {session: false}), catchAsync(postsController.likePostById));
 
 /**
  * @route POST api/posts/unlike/:id
  * @desc Unlikes post with specified id
  * @access Private
  **/
-router.post('/unlike/:id', passport.authenticate("jwt", {session: false}), postsController.unlikePostById);
+router.post('/unlike/:id', passport.authenticate("jwt", {session: false}), catchAsync(postsController.unlikePostById));
 
 /**
  * @route POST api/posts/comment/:id
  * @desc Add comment to post
  * @access Private
  **/
-router.post('/comment/:id', passport.authenticate("jwt", {session: false}), postsController.addCommentToPostById);
+router.post('/comment/:id', passport.authenticate("jwt", {session: false}),
+  catchAsync(postsController.addCommentToPostById));
 
 /**
  * @route DELETE api/posts/:id
  * @desc Deletes post by id
  * @access Private
  **/
-router.delete('/:id', passport.authenticate("jwt", {session: false}), postsController.deletePostById);
+router.delete('/:id', passport.authenticate("jwt", {session: false}), catchAsync(postsController.deletePostById));
 
 /**
  * @route DELETE api/posts/comment/:id/:comment_id
@@ -58,6 +60,6 @@ router.delete('/:id', passport.authenticate("jwt", {session: false}), postsContr
  * @access Private
  **/
 router.delete('/comment/:id/:comment_id', passport.authenticate("jwt", {session: false}),
-  postsController.deleteCommentByPostThenByCommentId);
+  catchAsync(postsController.deleteCommentByPostThenByCommentId));
 
 module.exports = router;
